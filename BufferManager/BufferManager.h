e83b04510791aea05f36c9d28f8ae50657892d0d
陈建瑜 <3150102769@zju.edu.cn>
2017-05-20 15:14:35 +0000

    添加 'BufferManager/BufferManager.h'

---
+++ BufferManager/BufferManager.h
@@ -0,0 +1,32 @@
+#ifndef _BUFFERMANAGER_H_
+#define _BUFFERMANAGER_H_
+
+#include <vector>
+#include <string>
+#include <cstdio>
+
+using namespace std
+
+class BufferManager{
+private:
+	vector<BufferUnit> buffers;
+	vector<string> files;
+	int BufferSize;
+	int BlockSize;
+
+	int getIndexByFileName(string filename); // Get the index of buffer in the buffers vector by filename.
+	ifstream::pos_type getFileSize(string filename); // Get the size of one file.
+	bool buildBuffer(string filename); // Build a buffer on a file.
+	bool destroyBuffer(string filename); // Destroy the specific file's buffer.
+
+public:
+	bool createFile(string filename);
+	bool deleteFile(string filename);
+	bool readDataFromFile(string filename, int blockIndex, char *readBuffer);
+	bool writeDataToFile(string filename, int blockIndex, char *writeBuffer);
+	bool lockBlock(string filename, int blockIndex);
+	BufferManager(int blockSize, int bufferSize);
+	~BufferManager();
+}
+
+#endif
