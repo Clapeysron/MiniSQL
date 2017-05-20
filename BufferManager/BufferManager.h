6eb67d3caacb2176a3a23740df91f6257550b0a4
陈建瑜 <3150102769@zju.edu.cn>
2017-05-20 15:19:33 +0000

    更新 'BufferManager/BufferManager.h'
    edit comment

--- BufferManager/BufferManager.h
+++ BufferManager/BufferManager.h
@@ -21,13 +21,13 @@ private:
 	bool destroyBuffer(string filename); // Destroy the specific file's buffer.
 
 public:
-	bool createFile(string filename);
-	bool deleteFile(string filename);
-	bool readDataFromFile(string filename, int blockIndex, char *readBuffer);
-	bool writeDataToFile(string filename, int blockIndex, char *writeBuffer);
-	bool lockBlock(string filename, int blockIndex);
-	BufferManager(int blockSize, int bufferSize);
-	~BufferManager();
+	bool createFile(string filename); // Create a file.
+	bool deleteFile(string filename); // Delete a file.
+	bool readDataFromFile(string filename, int blockIndex, char *readBuffer); // Read the specific blockIndex data in the file to the memory.
+	bool writeDataToFile(string filename, int blockIndex, char *writeBuffer); // Write the data to the specific blockIndex to the file from the memory.
+	bool lockBlock(string filename, int blockIndex); // Lock or Unlock the Block in the specific file.
+	BufferManager(int blockSize, int bufferSize); // Constructer
+	~BufferManager(); // Destructor
 }
 
 #endif
