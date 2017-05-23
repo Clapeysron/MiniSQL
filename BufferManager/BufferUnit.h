f6a428e0acb9305ea444021f508a7d079cd952cf
陈建瑜 <3150102769@zju.edu.cn>
2017-05-23 09:20:33 +0000

    添加 'BufferManager/BufferUnit.h'

---
+++ BufferManager/BufferUnit.h
@@ -0,0 +1,41 @@
+//
+// Created by Melody on 2017/5/23 0023.
+//
+
+#ifndef SQL_BUFFERUNIT_H
+#define SQL_BUFFERUNIT_H
+#include <fstream>
+#include <string>
+#include <vector>
+#include "BufferDataBlock.h"
+using namespace std;
+
+class BufferUnit {
+private:
+    int bufferSize = 256;
+    int blockSize = 4096;
+
+    string filename;
+    int filesize;
+    vector<int> blockIndexInBuffer;
+    vector<BufferDataBlock> blocks;
+
+    int clockIndex = 0;
+    int success = 0;
+
+    bool swapBlock(int fileindex, int blockindex);
+    int getValidBlock();
+    int upFloor(double size);
+
+public:
+    bool readBlock(int index, char *readBuffer);
+    bool writeBlock(int index, char*writeBuffer);
+    bool lockBlock(int index, int status);
+
+    BufferUnit(string filename, int blockSize, int bufferSize);
+    ~BufferUnit();
+
+};
+
+
+#endif //SQL_BUFFERUNIT_H
