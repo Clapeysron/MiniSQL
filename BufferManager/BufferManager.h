aa16963065e43e561f77be593185efedc1f8a51d
陈建瑜 <3150102769@zju.edu.cn>
2017-05-25 06:47:22 +0000

    更新了删除文件最后一个block的定义

--- BufferManager/BufferManager.h
+++ BufferManager/BufferManager.h
@@ -16,8 +16,8 @@ class BufferManager{
 private:
     vector<BufferUnit> buffers;
     vector<string> files;
-    int BufferSize;
-    int BlockSize;
+    int bufferSize;
+    int blockSize;
 
     int getIndexByFileName(string filename); // Get the index of buffer in the buffers vector by filename.
     ifstream::pos_type getFileSize(string filename); // Get the size of one file.
@@ -29,7 +29,8 @@ public:
     bool deleteFile(string filename); // Delete a file.
     bool readDataFromFile(string filename, int blockIndex, char *readBuffer); // Read the specific blockIndex data in the file to the memory.
     bool writeDataToFile(string filename, int blockIndex, char *writeBuffer); // Write the data to the specific blockIndex to the file from the memory.
-    bool lockBlock(string filename, int blockIndex); // lockBlockck or Unlock the Block in the specific file.
+    bool lockBlock(string filename, int blockIndex, int lock); // lockBlockck or Unlock the Block in the specific file.
+    bool deleteLastBlockOfFile(string filename);
     BufferManager(int blockSize, int bufferSize); // Constructor
     ~BufferManager(); // Destructor
 };
