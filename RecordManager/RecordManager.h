407e9ae2ac46cca261717b2b6c074f88effbe688
陈建瑜 <3150102769@zju.edu.cn>
2017-06-28 06:44:56 +0000

    更新 'RecordManager/RecordManager.h'

--- RecordManager/RecordManager.h
+++ RecordManager/RecordManager.h
@@ -7,6 +7,7 @@
 //#include "BufferManager.h"
 #include "../BufferManager/BufferManager.h"
 #include "TableStruct.h"
+#include "CharStream.h"
 
 #include <vector>
 #include <string>
@@ -17,6 +18,42 @@
 //#define GET_FILENAME(tn) (DATA_FILE_PATH + (tn) + DATA_FILE_EXTENSION)
 #define GET_FILENAME(tn) ((tn) + DATA_FILE_EXTENSION)
 
+#define LT 1	// <
+#define GT 2	// >
+#define NE 3	// <>
+#define EQ 4	// =
+#define LE 5	// <=
+#define GE 6	// >=
+
+class Condition {
+public:
+    string column;
+
+    int op;
+
+    string _str;
+    string _int;
+    string _flo;
+};
+#define COL 81
+#define STR 82
+#define INT 83
+#define DOUBLE 84
+
+// string 82, int 83, double 84
+#define COMPRASION	\
+{ \
+	switch (con.op) { \
+		case EQ: if (vala != valb) return false; break; \
+		case NE: if (vala == valb) return false; break; \
+		case LT: if (vala >= valb) return false; break; \
+		case GT: if (vala <= valb) return false; break; \
+		case LE: if (vala > valb) return false; break; \
+		case GE: if (vala < valb) return false; break; \
+		default: break; \
+	} \
+}
+
 class RecordManager {
 private:
     BufferManager &bm;
@@ -24,24 +61,23 @@ private:
     int getRecordLen(TableStruct &ts);
     bool isDup(TableStruct &ts, char* record);
     bool isDupEntry(vector<int> &pos, char *src, char *dst);
-	
 
 public:
-	RecordManager(BufferManager &bm, int blockSize) :bm(bm), blockSize(blockSize) {
-
-	};
-	~RecordManager() {
-		blockSize = 4096;
-	};
+    bool isConditionSatisfied(TableStruct &ts, Condition &con, char* str);
     bool createTable(string tableName);
     bool dropTable(string tableName);
     int insertIntoTable(TableStruct &ts, char* data);
-    int deleteRecord(TableStruct &ts, vector<int> &scope, vector<int> &moved);
+    int deleteRecord(TableStruct &ts, vector<int> &scope, vector<char *> &moved);
+    bool selectRecordWithCondition(TableStruct &ts, vector<int> &scope, vector<int> &results, int &comparison_type, int &type_1, string &comp_1, int &type_2, string comp_2);
     bool selectRecord(TableStruct &ts, vector<int> &scope, vector<char *> &result);
     bool selectAttribute(TableStruct &ts, string col, vector<char *> &values);
-	
 
-    
+    RecordManager(BufferManager &bm, int blockSize):bm(bm), blockSize(blockSize){
+
+    };
+    ~RecordManager(){
+        blockSize = 4096;
+    };
 };
 
 
